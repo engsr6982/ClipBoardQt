ab01711a5567efc414de89007162a82e8c19c5a8
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-06-28 09:48:51 +0800

    init qt

---
+++ xmake.lua
@@ -0,0 +1,27 @@
+add_rules("mode.release", "mode.debug")
+
+-- This Qt app:
+--    Windows:
+--      MSVC:
+--          xmake f -c --qt=C:\Qt\6.7.2\msvc2019_64
+--      Mingw:
+--          xmake f -p mingw -a x86_64 --qt=D:/software/Qt5.14.2/5.14.2/mingw73_64 --mingw=D:/software/Qt5.14.2/Tools/mingw730_64 --toolchain=mingw
+--    Linux:
+--      Todo
+
+
+if not has_config("vs_runtime") then 
+    set_runtimes("MD")
+end 
+
+
+target("ClipBoardQt")
+    add_rules("qt.widgetapp")
+
+    add_files(
+        "src/*.cpp",
+        "src/*.h",
+        "src/*.ui"
+    )
+    add_includedirs("src")
+    add_headerfiles("src/*.h")
